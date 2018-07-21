ceb8ca8bcfaaf539e08018d077c84be8f80a4835
Brian Zhang <brianzhangw@gmail.com>
2018-07-21 01:10:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function destructivelyAppendKitten(name){
   window.kittens.push(name);
 }
 
-function destuctivelyPrependKitten(name){
+function destructivelyPrependKitten(name){
   window.kittens.unshift(name);
 }
 
